a551ea06b6972d784d7b95fcabb553f10e44b207
Kushagra <kushagra100chhabra@gmail.com>
2025-07-15 16:41:17 +0530

    KJY

--- src/Controller/UserController.js
+++ src/Controller/UserController.js
@@ -1,11 +1,16 @@
 const UserModel = require("../Model/UserModel");
 const { otpVerificationUser } = require("../Mail/UserMail")
+const { errorHandlingdata } = require('../Error/ErrorHandling')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const dotenv = require("dotenv")
+dotenv.config()
 
 exports.createuser = async (req, res) => {
     try {
         const data = req.body;
 
-        if (Object.keys(data).length === 0) { return res.status(400).send({ status: false, msg: "Cannot provide empty field" }); }
+        if (Object.keys(data).length === 0) { return res.status(400).send({ status: false, msg: "A value is required in this field."}); }
 
         const randomOTP = Math.floor(1000 + Math.random() * 9000)
 
@@ -75,14 +80,59 @@ exports.UserOtpVerify = async (req, res) => {
         const user = await UserModel.findById(id);
         if (!user) return res.status(400).send({ status: true, msg: "User not found" });
         const dbOtp = user.Verification.user.UserOTP;
-
+        console.log(dbOtp, otp);
         if (!(dbOtp == otp)) return res.status(400).send({ status: true, msg: "Wrong otp" });
 
-        await UserModel.findByIdAndUpdate({ _id: id }, { $set: { 'Varification.user.isVerify': true } }, { new: true });
+        await UserModel.findByIdAndUpdate(
+            { _id: id },
+            { $set: { 'Verification.user.isVerify': true } },
+            { new: true }
+        );
         res.status(200).send({ status: true, msg: "User Verify successfully" });
 
     }
     catch (e) { errorHandlingdata(e, res) }
+}
+
+exports.LogInUser = async (req, res) => {
+    try {
+
+        const data = req.body
+        const { email, password } = data
+
+        const CheckUser = await UserModel.findOne({ email: email })
+        console.log(CheckUser.password); 
+
+        if (!CheckUser) return res.status(400).send({ status: false, msg: "User Not Found" })
+
+        const userVerification = CheckUser.Verification?.user || {};
+        const adminVerification = CheckUser.Verification?.admin || {};
+
+        const comparePass = await bcrypt.compare(password , CheckUser.password)
+        console.log(comparePass);
+        if(!comparePass) return res.status(400).send({status:false,  msg:"Wrong Password"})
+
+           if (CheckUser) {
+            console.log(CheckUser);
+            const DBDATABASE = { name: CheckUser.name, email: CheckUser.email, _id: CheckUser._id }
+
+            const userVerification = CheckUser.Verification?.user || {};
+            const adminVerification = CheckUser.Verification?.admin || {};
+
+            const { isDeleted, isVerify, isAccountActive } = userVerification
+            if (userVerification.isDeleted) return res.status(400).send({ status: false, msg: 'User already deleted' });
+            if (!userVerification.isVerify) return res.status(400).send({ status: false, msg: ' please verify your OTP' });
+            if (!adminVerification.isAccountActive) return res.status(400).send({ status: false, msg: 'User is blocked by admin' });          
+        }
+
+        const token = jwt.sign({userId : CheckUser._id} , process.env.JWT_User_SECRET_KEY , { expiresIn : '24h'})
+        return res.status(200).send({status:true , msg:"Login Successfully" , data:{token ,id: CheckUser._id}})
+    }
+
+    catch (e) {
+        errorHandlingdata(e, res)
+    }
+
 }
 
 exports.getUserById = async (req, res) => {
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 
-const { createuser ,  getUserById , UserOtpVerify} = require("../Controller/UserController");
+const { createuser ,  getUserById , UserOtpVerify , LogInUser} = require("../Controller/UserController");
 
 router.post('/createuser', createuser);
+router.post('/LogInUser', LogInUser);
 router.get('/getUserById/:id', getUserById);
 router.post('/user_otp_verify/:id', UserOtpVerify);
 
