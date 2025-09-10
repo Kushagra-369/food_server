f945dc23e6804c93cb9428c35119ce52bb0ea702
Kushagra <kushagra100chhabra@gmail.com>
2025-09-10 22:18:24 +0530

    GFD

--- src/Model/ProjectModel.js
+++ src/Model/ProjectModel.js
@@ -26,7 +26,7 @@ const productSchema = new mongoose.Schema(
         isAvailable: {type: Boolean,default: true},
         rating: { type: Number, default: 0, min: 0, max: 5 },
         reviews: [
-            {
+            { 
                 user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
                 comment: { type: String, trim: true },
                 rating: { type: Number, min: 1, max: 5, required: true },
